024c1bb394ab6d4b862fdafc8477c141f71d5238
Munir Mahmud <munirmahmud6@gmail.com>
2021-01-06 19:22:00 +0600

    Created initial express server and rendered home page

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts');
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send("Home Page");
+    res.render('home');
 });
+
+// Set template engine
+app.use(expressLayout);
+app.set('views', path.join(__dirname, '/client/resources/views'));
+app.set('view engine', 'ejs');
 
 app.listen(PORT, () => {
     console.log(`Server started http://localhost:${PORT}`);
